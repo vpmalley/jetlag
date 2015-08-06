83f612695cb56c8352c4c435f5d8ccdab3c44aee
Vincent Malley <vincent.malley@insalien.org>
2015-08-06 18:08:43 -0400

    added migration for users

---
+++ Laravel/database/migrations/2015_08_06_214727_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name', 200);
+            $table->string('email', 200)->unique();
+            $table->string('password', 100);
+            $table->string('remember_token', 100)->nullable();
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
