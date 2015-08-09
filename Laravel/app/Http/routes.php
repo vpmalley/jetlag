9f6e9612b0e023c4d26239f85b6a9cf6f31ee177
Vincent Malley <vincent.malley@insalien.org>
2015-08-09 12:17:52 -0400

    reshaping the routes file to list them all

--- Laravel/app/Http/routes.php
+++ Laravel/app/Http/routes.php
@@ -8,6 +8,8 @@
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
+| 
+| We should register each route explicitly to list here all endpoints we offer.
 |
 */
 
@@ -15,14 +17,19 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-  // Authentication routes...
+  // Authentication routes
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-// Registration routes...
+  // Registration routes
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+  // Traveller routes
+Route::get('traveller/{id}', 'Web\TravellerController@getSocial');
+Route::get('traveller/{id}/social', 'Web\TravellerController@getSocial');
+Route::get('traveller/{id}/edit', 'Web\TravellerController@getEdit');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
