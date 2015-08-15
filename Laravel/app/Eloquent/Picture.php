5f9d6cfc1a44ecf46057924df524f0d3cc8a1168
Vincent Malley <vincent.malley@insalien.org>
2015-08-15 08:42:43 -0400

    adding Picture model

---
+++ Laravel/app/Eloquent/Picture.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Jetlag\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Picture extends Model
+{
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'pictures';
+
+	/**
+	 * The attributes that are mass assignable.
+   * A picture has so far 3 representations of different sizes (we can add more)
+   * authorId refers (in authors table) to who created/can manage the picture
+   * locationId refers to the place the picture was taken
+   *  
+	 * @var array
+	 */
+	protected $fillable = ['smallPictureLinkId', 'mediumPictureLinkId', 'bigPictureLinkId', 'authorId', 'locationId'];
+}
---
+++ Laravel/database/migrations/2015_08_15_123217_create_pictures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->bigInteger('smallPictureLinkId');
+            $table->bigInteger('mediumPictureLinkId');
+            $table->bigInteger('bigPictureLinkId');
+            $table->bigInteger('authorId');
+            $table->bigInteger('locationId');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pictures');
+    }
+}
