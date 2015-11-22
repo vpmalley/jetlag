e6c7f096fbfde9071b5b25924189b50f97762973
Vincent Malley <vincent.malley@insalien.org>
2015-11-22 11:16:24 +0100

    #9 fix redirection when hitting the homepage

--- Laravel/app/Http/Middleware/RedirectIfAuthenticated.php
+++ Laravel/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,44 +6,40 @@ use Illuminate\Http\RedirectResponse;
 
 class RedirectIfAuthenticated {
 
-	/**
-	 * The Guard implementation.
-	 *
-	 * @var Guard
-	 */
-	protected $auth;
+  /**
+  * The Guard implementation.
+  *
+  * @var Guard
+  */
+  protected $auth;
 
-	/**
-	 * Create a new filter instance.
-	 *
-	 * @param  Guard  $auth
-	 * @return void
-	 */
-	public function __construct(Guard $auth)
-	{
-		$this->auth = $auth;
-	}
+  /**
+  * Create a new filter instance.
+  *
+  * @param  Guard  $auth
+  * @return void
+  */
+  public function __construct(Guard $auth)
+  {
+    $this->auth = $auth;
+  }
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		if ($this->auth->check())
-		{
-			if ($this->auth->user()) 
-			{
-				$url = '/traveller/' . $this->auth->user()->id . '/edit';
-				return new RedirectResponse(url($url));
-			}
-				return new RedirectResponse(url('/home'));
-			}
+  /**
+  * Handle an incoming request.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  \Closure  $next
+  * @return mixed
+  */
+  public function handle($request, Closure $next)
+  {
+    // redirect to /home if authenticated
+    if ($this->auth->check() && ('home' != $request->path()))
+    {
+      return new RedirectResponse(url('/home'));
+    }
 
-		return $next($request);
-	}
+    return $next($request);
+  }
 
 }
