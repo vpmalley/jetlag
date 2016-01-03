24bd61e2a1532c355fbb1d0780ab90967f63b434
Vallenain <vallenain@free.fr>
2016-01-03 20:56:31 +0100

    added first steps of travelbook creation process #22

--- Laravel/resources/assets/js/travelbookCreator.js
+++ Laravel/resources/assets/js/travelbookCreator.js
@@ -6,6 +6,18 @@ TravelbookCreatorController.$inject = ['$scope', 'ModelsManager'];
 
 function TravelbookCreatorController($scope, ModelsManager) {
 	var ctrl = this;
+	ctrl.step = 1;
 	
+	ctrl.changeStep = function(stepNumber) {
+		if(_.isNumber(stepNumber)) {
+			ctrl.step = stepNumber;
+		} else {
+			if(ctrl.step < 5) {
+				ctrl.step += 1;
+			} else {
+				console.log('end of process!');
+			}
+		}
+	}
 };
 
--- Laravel/resources/views/travelbookCreator.blade.php
+++ Laravel/resources/views/travelbookCreator.blade.php
@@ -9,7 +9,49 @@
 @endsection
 
 @section('content')
-<div class="jl-tbCreator" ng-controller="TravelbookCreatorController as tbCreatorCtrl">
+<div class="jl-tbCreator" ng-controller="TravelbookCreatorController as tbCreatorCtrl"
+	style="margin-top: 150px">
+	<div class="step" ng-class="{'step-open': tbCreatorCtrl.step === 1}">
+		<div class="step-header">
+			<div class="step-number pull-left">
+				1
+			</div>
+			<div class="step-skip pull-right">
+				<span ng-click="tbCreatorCtrl.changeStep()">PASSER</span>
+			</div>
+			<div class="step-expand pull-right" ng-click="tbCreatorCtrl.changeStep(1)">
+				<i class="fa fa-fw fa-chevron-down"></i>
+			</div>
+			<div class="step-title center-block">
+				Par quoi commencer ?
+			</div>
+		</div>
+		<div class="step-body clearfix">
+			<p>Infos du carnet</p>
+			<p>ou</p>
+			<p>1er article</p>
+		</div>
+	</div>
+	<div class="step" ng-class="{'step-open': tbCreatorCtrl.step === 2}">
+		<div class="step-header">
+			<div class="step-number pull-left">
+				2
+			</div>
+			<div class="step-skip pull-right">
+				<span ng-click="tbCreatorCtrl.changeStep()">PASSER</span>
+			</div>
+			<div class="step-expand pull-right" ng-click="tbCreatorCtrl.changeStep(2)">
+				<i class="fa fa-fw fa-chevron-down"></i>
+			</div>
+			<div class="step-title center-block">
+				Dates
+			</div>
+		</div>
+		<div class="step-body clearfix">
+			<p>Quand le voyage a-t-il commencé ?</p>
+			<p>12/12/12</p>
+		</div>
+	</div>
 </div>
 @endsection
 
