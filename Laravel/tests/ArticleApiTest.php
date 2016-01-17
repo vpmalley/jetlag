72516b6a0a8c3144ae562fd9907e0f6c997a0d26
Vincent Malley <vincent.malley@insalien.org>
2016-01-17 17:53:32 +0100

    fixing tests

--- Laravel/tests/ArticleApiTest.php
+++ Laravel/tests/ArticleApiTest.php
@@ -489,6 +489,9 @@ class ArticleApiTest extends TestCase {
         'id' => 1,
         'title' => 'A first paragraph',
         'block_content' => [
+          'id' => 1,
+          'smallUrl' => null,
+          'mediumUrl' => null,
           'big_url' => [
             'caption' => '',
             'url' => 'http://s2.lemde.fr/image2x/2015/11/15/92x61/4810325_7_5d59_mauri7-rue-du-faubourg-saint-denis-10e_86775f5ea996250791714e43e8058b07.jpg',
