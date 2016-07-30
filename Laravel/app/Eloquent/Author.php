48de3261c7b7f682525e834c2922aabacee32746
Vallenain <vallenain@free.fr>
2016-07-30 09:42:58 +0000

    unresolved git conflicts #46

--- Laravel/app/Eloquent/Author.php
+++ Laravel/app/Eloquent/Author.php
@@ -49,11 +49,10 @@ class Author extends Model
   public static function isOwner($userId, $authorId)
   {
     // the number of rows matching the authenticated user and element's author : either 0 or 1
-<<<<<<< HEAD
+/* XXX: was in 201601-creators --
     $count = Author::where('userId', $userId)->where('authorId', $authorId)->where('role', self::ROLE_OWNER)->count();
-=======
+*/
     $count = Author::where('user_id', $userId)->where('author_id', $authorId)->where('role', self::K_OWNER)->count();
->>>>>>> develop
     return (1 == $count);
   }
 
@@ -65,17 +64,16 @@ class Author extends Model
   public static function isWriter($userId, $authorId)
   {
     // the number of rows matching the authenticated user and element's author : either 0 or 1
-<<<<<<< HEAD
+/* XXX: was in 201601-creators --
     $count = Author::where('userId', $userId)->where('authorId', $authorId)->where(function ($query) {
                 $query->where('role', self::ROLE_OWNER)
                       ->orWhere('role', self::ROLE_WRITER);
             })->count();
-=======
+*/
     $count = Author::where('user_id', $userId)->where('author_id', $authorId)->where(function ($query) {
       $query->where('role', self::K_OWNER)
       ->orWhere('role', self::K_WRITER);
     })->count();
->>>>>>> develop
     return (1 == $count);
   }
 
