35497932e3dab1ca19a6e3dd10bd7b4569745493
Valentin Coudert <vallenain@free.fr>
2016-09-17 18:06:22 +0200

    added tabs in user profile

--- Laravel/resources/views/web/user/display.blade.php
+++ Laravel/resources/views/web/user/display.blade.php
@@ -6,37 +6,50 @@
 
 @section('content')
 <div class="container-fluid">
+    
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{$name}}'s public information</div>
-				<div class="panel-body">
-
-          <div class="form-horizontal">
-            <div class="form-group">
-              <label class="col-md-4 control-label">Public name</label>
-              <div class="col-md-6">
-                <label class="col-md-4 control-label">{{$name}}</label>
-              </div>
+        <ul class="nav nav-pills" role="tablist">
+          <li role="presentation" class="active"><a href="#travelbooks" aria-controls="travelbooks" role="tab" data-toggle="pill">Travelbooks</a></li>
+          <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="pill">Profile</a></li>
+        </ul>
+    
+        <div class="col-md-12 tab-content">
+            <div class="row tab-pane" role="tabpanel" id="travelbooks">
             </div>
+            
+            <div class="row tab-pane" role="tabpanel" id="profile">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">{{$name}}'s public information</div>
+                        <div class="panel-body">
 
-            <div class="form-group">
-              <label class="col-md-4 control-label">Country</label>
-              <div class="col-md-6">
-                <label class="col-md-4 control-label">{{$country}}</label>
-              </div>
-            </div>
+                  <div class="form-horizontal">
+                    <div class="form-group">
+                      <label class="col-md-4 control-label">Public name</label>
+                      <div class="col-md-6">
+                        <label class="col-md-4 control-label">{{$name}}</label>
+                      </div>
+                    </div>
 
-            <div class="form-group">
-              <label class="col-md-4 control-label">City</label>
-              <div class="col-md-6">
-                <label class="col-md-4 control-label">{{$city}}</label>
-              </div>
+                    <div class="form-group">
+                      <label class="col-md-4 control-label">Country</label>
+                      <div class="col-md-6">
+                        <label class="col-md-4 control-label">{{$country}}</label>
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <label class="col-md-4 control-label">City</label>
+                      <div class="col-md-6">
+                        <label class="col-md-4 control-label">{{$city}}</label>
+                      </div>
+                    </div>
+                  </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-				</div>
-			</div>
-		</div>
+        </div>
 	</div>
 </div>
 @endsection
--- Laravel/resources/views/web/user/edit.blade.php
+++ Laravel/resources/views/web/user/edit.blade.php
@@ -7,69 +7,82 @@
 @section('content')
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default" style="margin-top: 20px">
-				<div class="panel-heading">You can edit your public information</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-          
-					@if ($saved)
-						<div class="alert alert-success">
-							Your changes have been saved.
-						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/traveller/' . $id . '/edit') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Public name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{$name}}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Country</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="country" value="{{$country}}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">City</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="city" value="{{$city}}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Profile picture url</label>
-							<div class="col-md-6">
-								<input type="url" class="form-control" name="Profile picture">
-							</div>
-						</div>
+    
+        <ul class="nav nav-pills" role="tablist">
+          <li role="presentation" class="active"><a href="#travelbooks" aria-controls="travelbooks" role="tab" data-toggle="pill">Travelbooks</a></li>
+          <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="pill">Profile</a></li>
+        </ul>
+        
+        <div class="col-md-12 tab-content">
+            <div class="row tab-pane" role="tabpanel" id="travelbooks">
+            </div>
             
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="jl-btn">
-									Save
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
+            <div class="row tab-pane" role="tabpanel" id="profile">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default" style="margin-top: 20px">
+                        <div class="panel-heading">You can edit your public information</div>
+                        <div class="panel-body">
+                            @if (count($errors) > 0)
+                                <div class="alert alert-danger">
+                                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+                  
+                            @if ($saved)
+                                <div class="alert alert-success">
+                                    Your changes have been saved.
+                                </div>
+                            @endif
+
+                            <form class="form-horizontal" role="form" method="POST" action="{{ url('/traveller/' . $id . '/edit') }}">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label">Public name</label>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" name="name" value="{{$name}}">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label">Country</label>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" name="country" value="{{$country}}">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label">City</label>
+                                    <div class="col-md-6">
+                                        <input type="text" class="form-control" name="city" value="{{$city}}">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label">Profile picture url</label>
+                                    <div class="col-md-6">
+                                        <input type="url" class="form-control" name="Profile picture">
+                                    </div>
+                                </div>
+                    
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <button type="submit" class="jl-btn">
+                                            Save
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 	</div>
 </div>
 @endsection
